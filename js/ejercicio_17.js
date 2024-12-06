e020f17275509189275dfbedb49ad9364ceb0a98
Valerieee01 <zharmel.afa@gmail.com>
2024-12-06 10:53:05 -0500

    ejercicio 17

---
+++ js/ejercicio_17.js
@@ -0,0 +1,8 @@
+//Escribir un programa que calcule el volumen de una esfera.
+const pi = 3.1416
+let radio = parseFloat(prompt("ingrese el radio del cilindro"));
+function calcular(radio){
+    let v = 3 / 4 * pi * radio * radio * radio;
+    alert("el volumen de una esfera es : "+v);
+}
+let rta = calcular(radio);

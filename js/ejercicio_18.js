3c6ee577bb6ced8fc926493c992f3ef526a86482
Valerieee01 <zharmel.afa@gmail.com>
2024-12-06 10:57:11 -0500

    ejercicio 18

---
+++ js/ejercicio_18.js
@@ -0,0 +1,6 @@
+//Escribir un programa que evalúe la siguiente expresión (a+7*c) / (b+2-a) + 2*b
+let a = parseInt(prompt("ingrese el valor de a : "));
+let b = parseInt(prompt("ingrese el valor de b : "));
+let c = parseInt(prompt("ingrese el valor de c : "));
+let calcular = (a+7*c) / (b+2-2) + 2*b;
+alert("el resultado de la exprecion (a+7*c) / (b+2-a) + 2*b es : "+calcular);

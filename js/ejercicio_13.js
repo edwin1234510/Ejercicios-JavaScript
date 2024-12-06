50c331191b787d9afab7d2a122eb46db2bbfae5d
Valerieee01 <zharmel.afa@gmail.com>
2024-12-06 10:17:05 -0500

    ejercicio 13

---
+++ js/ejercicio_13.js
@@ -0,0 +1,9 @@
+//Escribir un programa que lea 4 números y calcule la media.
+let num1,num2,num3,num4;
+let media;
+num1 = parseInt(prompt("ingrese el numero 1 : "));
+num2 = parseInt(prompt("ingrese el numero 2 : "));
+num3 = parseInt(prompt("ingrese el numero 3 : "));
+num4 = parseInt(prompt("ingrese el numero 4 : "));
+media = (num1 + num2 + num3 + num4) / 4
+alert("la media es : "+media);

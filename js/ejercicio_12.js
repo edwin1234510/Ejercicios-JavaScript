da123e082164da8166e37a67e2710bb163f7ebdd
Valerieee01 <zharmel.afa@gmail.com>
2024-12-06 09:54:21 -0500

    ejercicio 12

---
+++ js/ejercicio_12.js
@@ -0,0 +1,9 @@
+//Escribir un programa que calcule el área de un rectángulo:
+let base,altura;
+base = prompt("ingrese la base : ");
+altura = prompt("ingrese la altura : ");
+function area(base,altura){
+    let calular = base * altura;
+    alert("el area es : "+calular);
+}
+let rta = area(base,altura);

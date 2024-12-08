8057dd86d9eb9d22edb8849d8dbe1f205550ad59
edwin <edwinvillabona0701@gmail.com>
2024-12-08 17:30:01 -0500

    ejercicio 35 36

---
+++ js/ejercicio_35.js
@@ -0,0 +1,7 @@
+let llaves = 11500;
+let bomba = 1168000;
+let pernos = 87000;
+let sobrante = 91000;
+let calcular = (llaves * 5) + bomba + (pernos * 3) + sobrante;
+alert("tiene de dinero : $ "+calcular);
+console.log(calcular);
---
+++ js/ejercicio_36.js
@@ -0,0 +1,4 @@
+let chocolate = 20;
+let harina = 20 * 10;
+alert("la cantidad de harina es : "+harina);
+console.log("la cantidad de harina es : "+harina);

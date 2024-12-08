2459feb508fe10b31d1e1c5170dd94b895d84ede
edwin <edwinvillabona0701@gmail.com>
2024-12-08 17:54:40 -0500

    ejercicio 38

---
+++ js/ejercicio_38.js
@@ -0,0 +1,10 @@
+// martin 2/3
+//Jairo 1/4
+let n1 = 2 * 4;
+//------------------
+let n2 = 1 * 3;
+let n3 = 3 * 4;
+let suma = n1 + n2;
+let lorena = n3 - suma;
+alert("a lorena le corresponde "+lorena+"/"+n3);
+console.log("a lorena le corresponde "+lorena+"/"+n3);

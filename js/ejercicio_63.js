434805e8220406acb0089a4dbe62c31aea7fa446
edwin <edwinvillabona0701@gmail.com>
2024-12-09 16:33:05 -0500

    ejercicio 63

---
+++ js/ejercicio_63.js
@@ -0,0 +1,9 @@
+let añoActual = 2024;
+let edad = parseInt(prompt("ingrese su edad : "));
+let arreglo = [];
+for(let i = 1; i <= edad; i++){
+    arreglo.push(i);
+}
+alert(arreglo);
+console.log(arreglo);
+

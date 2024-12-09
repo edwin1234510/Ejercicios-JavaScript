bdd98c4723fc0d6b8216c7a74dfd728b185b6856
edwin <edwinvillabona0701@gmail.com>
2024-12-09 16:02:35 -0500

    ejercicio 60

---
+++ js/ejercicio_60.js
@@ -0,0 +1,7 @@
+let cursos = parseInt(prompt("ingrese la candidad de cursos :"));
+if (cursos <= 6){
+    alert(" El pago será $2.000.000");
+}
+if (cursos > 6){
+    alert("El pago será $1.200.000");
+}

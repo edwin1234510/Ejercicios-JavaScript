fbdddb74391bb5a7934eef23187a8f49f790bf85
edwin <edwinvillabona0701@gmail.com>
2024-12-10 13:36:41 -0500

    ejercicio 73

---
+++ js/ejercicio_73.js
@@ -0,0 +1,12 @@
+//Escribir un programa en el que se pregunte al usuario por una frase y una letra, y muestre
+//por pantalla el número de veces que aparece la letra en la frase.
+// Pedir al usuario una frase y una letra
+let frase = prompt("Introduce una frase:");
+let letra = prompt("Introduce una letra:");
+let contador = 0;
+for (let i = 0; i < frase.length; i++) {
+    if (frase[i] === letra) {
+        contador++;
+    }
+}
+alert("La letra '" + letra + "' aparece " + contador + " veces en la frase.");
